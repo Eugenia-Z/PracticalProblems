21da9b8d72bf818e4c47f5cfec9593529478f7d6
eugenia <e.eugene1201@gmail.com>
2024-10-18 13:02:57 -0700

    max readings gpt sol

--- Uber OA_CodeSignalGeneral/ReadingFromSensors.py
+++ Uber OA_CodeSignalGeneral/ReadingFromSensors.py
@@ -23,8 +23,26 @@ def solution(readings):
     return list(freq_sorted.keys())[0]
 
 # GPT's solution
+# Note that: to deal with sorting in case of a tie, just pass a tuple to lambda function -> key=lambda x:(frequency[x], x)
+
+from collections import Counter
+def digit_root(num):
+    while num >= 10:
+        num = sum(int(digit) for digit in str(num))
+    return num
+def solution2(readings):
+    # Step 1: transform each number to its digit root (single digit form)
+    transformed = [digit_root(num) for num in readings]  # calling the helper function on the go
+    
+    # Step2: counting the occurrences of each digit
+    frequency = Counter(transformed)
+    #print(frequency)
+
+    # Step3: Find the most frequent digit. In case of a tie, return the highest digit
+    most_frequent_digit = max(frequency, key=lambda x:(frequency[x], x))   
+    return most_frequent_digit
 
 if __name__ == "__main__":
     readings = [123,456,789,101]
-    print(solution(readings))
+    print(solution2(readings))
 
