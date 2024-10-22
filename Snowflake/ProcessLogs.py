8ccdcd20ef1078f7cd76ff5992dba51cc75e3916
eugenia <e.eugene1201@gmail.com>
2024-10-22 09:32:06 -0700

    update sort

--- Snowflake/ProcessLogs.py
+++ Snowflake/ProcessLogs.py
@@ -19,9 +19,13 @@ def processLogs(logs, threshold):
             
     # Filter out the users whose transaction count meets or exceeds the threshold
     res = [user for user, count in trans_logs.items() if count >= threshold]
-
+    
     # .sort() methods sorts the list in place but returns None. should sort the list and return it instead.
-    return sorted(res)
+    # return sorted(res)
+    
+    # Alternatively, sort the user IDs as strings numerically (convert to int for correct sorting)
+    res.sort(key=int)
+    return res
 
 if __name__ == "__main__":
     logs = ["88 99 200", "88 99 300", "99 32 100", "12 12 15"]
